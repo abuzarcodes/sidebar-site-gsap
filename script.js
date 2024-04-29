2acecd8f9a0429d0527deb5b8423699da69a8e71
abuzarcodes <abuzar200496@gmail.com>
2024-04-29 21:52:34 +0530

    sidebar

---
+++ script.js
@@ -0,0 +1,35 @@
+const closeBtn = document.querySelector(".ri-close-circle-line");
+const openBtn = document.querySelector("nav i");
+
+gsap.from("nav h1", {
+  y: -30,
+  opacity: 0,
+  delay: 0.3,
+  duration: 0.7,
+});
+gsap.from("nav i", {
+  y: -30,
+  opacity: 0,
+  delay: 0.3,
+  duration: 0.7,
+});
+openBtn.addEventListener("click", function () {
+  var tl = gsap.timeline();
+  tl.to(openBtn, {
+    display: "none",
+    
+  });
+  tl.to(".sidebar", {
+    right: 0,
+    duration: 0.4,
+  });
+  tl.from(".sidebar h1", {
+    x: 200,
+    opacity: 0,
+    duration: 0.2,
+    stagger: 0.3,
+  });
+  closeBtn.addEventListener("click", function () {
+    tl.reverse();
+  });
+});
